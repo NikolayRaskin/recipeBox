6dea78016e66473534fa0d98bc1fb54e07cb0716
Nikolay Raskin <n.woots98@gmail.com>
2019-07-07 22:05:01 +0300

    registration(finished)

--- recipeBox/mainPage/regForm.py
+++ recipeBox/mainPage/regForm.py
@@ -3,4 +3,5 @@ from django import forms
 class RegForm(forms.Form):
     user_name = forms.CharField(label='Логин:', max_length=100,widget=forms.TextInput(attrs={'placeholder': 'Логин'}))    
     user_email = forms.EmailField(label='Email:',widget=forms.TextInput(attrs={'placeholder': 'Email'}))   
-    password = forms.CharField(label='Пароль:',widget=forms.PasswordInput())
+    password = forms.CharField(label='Пароль:',widget=forms.PasswordInput())    
+    confirm_password = forms.CharField(label='Подтвердите пароль:',widget=forms.PasswordInput())
--- recipeBox/mainPage/views.py
+++ recipeBox/mainPage/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponseRedirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .regForm import RegForm
+
+from django.contrib import messages
+
 
 def mainPage(request):
     return render(request,'mainPage/mainPage.html')
@@ -15,13 +18,35 @@ def register(request):
         user_email = 0
         user_pass = 0
         if form.is_valid():
-            #form.save()
             user_name = form.cleaned_data['user_name']
             user_email = form.cleaned_data['user_email']
             user_pass = form.cleaned_data['password']
-            user = User.objects.create_user(user_name, user_email, user_pass)
-            login(request,user)
-            return redirect('/')
+            user_pass_confirm = form.cleaned_data['confirm_password']
+            
+            if username_exists(user_name):
+                messages.info(request, 'Логин занят!')
+                return HttpResponseRedirect('/register/')
+            elif email_exists(user_email):
+                messages.info(request, 'Email занят!')
+                return HttpResponseRedirect('/register/')
+            elif user_pass != user_pass_confirm:
+                messages.info(request, 'Заданный пароль и подтверждение пароля не совпадают!')
+                return HttpResponseRedirect('/register/')
+            else:
+                user = User.objects.create_user(user_name, user_email, user_pass)
+                login(request,user)
+                return redirect('/')
     else:
         form = RegForm()
     return render(request,'registration/register.html',{'form':form})
+
+def username_exists(user_name):
+    if User.objects.filter(username=user_name).exists():
+        return True
+    else:
+        return False
+def email_exists(user_email):
+    if User.objects.filter(email=user_email).exists():
+        return True
+    else:
+        return False
