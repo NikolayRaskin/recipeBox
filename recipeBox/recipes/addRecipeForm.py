eff721f7d1590f659ec408a7f1cd0dd143e17239
Nikolay Raskin <n.woots98@gmail.com>
2019-07-10 22:33:36 +0300

    formLogic

--- recipeBox/recipes/addRecipeForm.py
+++ recipeBox/recipes/addRecipeForm.py
@@ -21,13 +21,13 @@ class AddRecipeForm(forms.Form):
     kindOfDish = forms.ChoiceField(widget=forms.Select,choices=KINDOFDISH)
     
     typeOfDish = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICESOFDISH)
-    kindOfMeat = forms.ChoiceField(widget=forms.Select,choices=MEAT_CHOICES)
-    kindOfFish = forms.CharField(max_length=100)
-    nameOfIngrediants = forms.CharField(max_length=200,widget=forms.TextInput(attrs={'placeholder': 'Ингредиент'}))
-    countOfIngrediants = forms.IntegerField()
+    kindOfMeat = forms.ChoiceField(widget=forms.Select,choices=MEAT_CHOICES,required=False)
+    kindOfFish = forms.CharField(max_length=100,required=False)
+    nameOfIngrediants = forms.CharField(max_length=200,widget=forms.TextInput(attrs={'placeholder': 'Ингредиент'}),required=False)
+    countOfIngrediants = forms.IntegerField(required=False)
     instruction = forms.CharField(max_length=5000, widget=forms.Textarea)
-    description = forms.CharField(max_length=5000, widget=forms.Textarea)
-    oursForCooking = forms.IntegerField()
+    description = forms.CharField(max_length=5000, widget=forms.Textarea,required=False)
+    oursForCooking = forms.IntegerField(required=False)
     minutsForCooking = forms.IntegerField()
     
     
