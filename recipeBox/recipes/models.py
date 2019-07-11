cc5c4d7459b2664e44bf96857101d1089297fa3d
Nikolay Raskin <n.woots98@gmail.com>
2019-07-11 23:20:22 +0300

    adding to db

--- recipeBox/recipes/models.py
+++ recipeBox/recipes/models.py
@@ -3,12 +3,6 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 
-class Country(models.Model):
-    country = models.CharField(max_length=100)
-    def __str__(self):
-        return self.country
-
-
 class PostBase(models.Model):
     class Meta:
         abstract = True
@@ -23,21 +17,19 @@ class PostBase(models.Model):
         (POST_MODERATED, 'Post Moderated'),
         (MODERATED, 'Moderated')
     )
-    LEVEL_CHOICES = (
-        ('L1', 'Уровень 1'),
-        ('L2', 'Уровень 2'),
-        ('L3', 'Уровень 3'),
-    )
  
-    author = models.ForeignKey(User, verbose_name=_("Автор"),on_delete=models.PROTECT)
-    title = models.CharField(_('Заголовок'),max_length=100)
-    ingredients = models.TextField(_('Ингредиенты'))
-    cookingDescription = models.TextField(_('Рецепт'))
-    cookingDifficulty = models.CharField(_('Уровень сложности'),choices=LEVEL_CHOICES,max_length=100)
-    preparationTime = models.IntegerField(_('Время изготовления'))
-    img = models.ImageField(_('Изображение'),upload_to='recipes/static/recipes/img/')
-    country = models.ForeignKey(Country,on_delete=models.PROTECT)
-    pub_date = models.DateTimeField(_('Дата публикации'), blank=True, null=True,auto_now=True)
+    author = models.ForeignKey(User, verbose_name=_("Автор"),on_delete=models.PROTECT,null=True)
+    title = models.CharField(_('Заголовок'),max_length=100,default=None)
+    kindOfDish = models.CharField(_('Категория блюда'),max_length=100,default=None)
+    ingredients = models.TextField(_('Ингредиенты'),default='default',null=True)
+    instruction = models.TextField(_('Инструкция'),default='default',null=True)
+    description = models.TextField(_('Примечание'),default='default',null=True)
+    level = models.CharField(_('Уровень сложности'),max_length=100,default=None)
+    oursForCooking = models.IntegerField(_('Час(ов)'),default=None)
+    minutsForCooking = models.IntegerField(_('Минут(ы)'),default=None)
+    #img = models.ImageField(_('Изображение'),upload_to='recipes/static/recipes/img/')
+    country = models.CharField(_('Происхождение'),max_length=100,default=None)
+    pub_date = models.DateTimeField(_('Дата публикации'), blank=True,null=True,auto_now=True)
     moderation = models.CharField(
         _('Модерация'),
         max_length=1,
@@ -46,32 +38,17 @@ class PostBase(models.Model):
     )
 
 class MeatDish(PostBase):
-    class Meta:
-        db_table = "MeatDish"
-    MEAT_CHOICES = (
-        ('CHICKEN','Курица'),
-        ('COW','Говядина'),
-        ('PIG','Свинина'),
-        ('RAM','Баранина'),
-        ('VEAL','Телятина'),
-        ('TURTLE','Черепаха'),
-    )
-    kindOfMeat = models.CharField(choices=MEAT_CHOICES,max_length=100)
+    kindOfMeat = models.CharField(max_length=100,default=None,null=True)
         
     def __str__(self):
         return self.title
 
-class FishDish(PostBase):
-    class Meta:
-        db_table = "FishDish"
-    
-    kindOfFish = models.CharField(max_length=100)
+class FishDish(PostBase): 
+    kindOfFish = models.CharField(max_length=100,default=None,null=True)
         
     def __str__(self):
         return self.title
 
-class VeganDish(models.Model):
-    class Meta:
-        db_table = "VeganDish"
+class VeganDish(PostBase):
     def __str__(self):
         return self.title
--- recipeBox/recipes/views.py
+++ recipeBox/recipes/views.py
@@ -1,34 +1,60 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .addRecipeForm import AddRecipeForm
+from .models import MeatDish,FishDish,VeganDish
 
 @login_required
 def addRecipe(request):
     if request.method == 'POST':
         form = AddRecipeForm(request.POST)
         if form.is_valid():
+            ings = []
+            
             title = form.cleaned_data['title']
+            country = form.cleaned_data['country']
             kindOfDish = form.cleaned_data['kindOfDish']
             typeOfDish = form.cleaned_data['typeOfDish']
             kindOfMeat = form.cleaned_data['kindOfMeat']
             kindOfFish = form.cleaned_data['kindOfFish']
-            nameOfIngrediants = form.cleaned_data['nameOfIngrediants']
-            countOfIngrediants = form.cleaned_data['countOfIngrediants']
+            
+            #-----------------------------------
+            names=[form.cleaned_data['nameOfIngrediants1'],form.cleaned_data['nameOfIngrediants2'],form.cleaned_data['nameOfIngrediants3'],form.cleaned_data['nameOfIngrediants4'],form.cleaned_data['nameOfIngrediants5'],form.cleaned_data['nameOfIngrediants6'],form.cleaned_data['nameOfIngrediants7'],form.cleaned_data['nameOfIngrediants8'],form.cleaned_data['nameOfIngrediants9'],form.cleaned_data['nameOfIngrediants10']]
+            counts=[form.cleaned_data['countOfIngrediants1'],form.cleaned_data['countOfIngrediants2'],form.cleaned_data['countOfIngrediants3'],form.cleaned_data['countOfIngrediants4'],form.cleaned_data['countOfIngrediants5'],form.cleaned_data['countOfIngrediants6'],form.cleaned_data['countOfIngrediants7'],form.cleaned_data['countOfIngrediants8'],form.cleaned_data['countOfIngrediants9'],form.cleaned_data['countOfIngrediants10']]
+            calcs=[form.cleaned_data['calcUnitOfIngrediants1'],form.cleaned_data['calcUnitOfIngrediants2'],form.cleaned_data['calcUnitOfIngrediants3'],form.cleaned_data['calcUnitOfIngrediants4'],form.cleaned_data['calcUnitOfIngrediants5'],form.cleaned_data['calcUnitOfIngrediants6'],form.cleaned_data['calcUnitOfIngrediants7'],form.cleaned_data['calcUnitOfIngrediants8'],form.cleaned_data['calcUnitOfIngrediants9'],form.cleaned_data['calcUnitOfIngrediants10']]
+            #-----------------------------------
+            
             instruction = form.cleaned_data['instruction']
             description = form.cleaned_data['description']
             oursForCooking = form.cleaned_data['oursForCooking']
             minutsForCooking = form.cleaned_data['minutsForCooking']
-            print(title)
-            print(kindOfDish)
-            print(typeOfDish)
-            print(kindOfMeat)
-            print(kindOfFish)
-            print(nameOfIngrediants)
-            print(countOfIngrediants)
-            print(instruction)
-            print(description)
-            print(oursForCooking)
-            print(minutsForCooking)
+            level = form.cleaned_data['level']
+            
+            for i in range(len(names)):
+                ings.append({str(i) + '. ' + str(names[i]):str(counts[i]) + ' ' + calcs[i]})
+            
+            if typeOfDish == 'Мясное':
+                new_recipe = MeatDish()
+                new_recipe.kindOfMeat = kindOfMeat
+            elif typeOfDish == 'Рыбное':
+                new_recipe = FishDish()
+                new_recipe.kindOfFish = kindOfFish
+            elif typeOfDish == 'Вегетарианское':
+                new_recipe = VeganDish()
+                
+            new_recipe.author = request.user
+            new_recipe.title = title
+            new_recipe.country = country
+            new_recipe.kindOfDish = kindOfDish
+            new_recipe.ingredients = ings
+            new_recipe.instruction = instruction
+            new_recipe.description = description
+            new_recipe.oursForCooking = oursForCooking
+            new_recipe.minutsForCooking = minutsForCooking
+            new_recipe.level = level
+            new_recipe.save()
+            
+            
+            
             return redirect('/user_profile/')
         else:
             print('not valid')
